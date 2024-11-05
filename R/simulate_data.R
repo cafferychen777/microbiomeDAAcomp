4a34a95c4d2e147f4fb01cc774c4c799eae694e8
Chen Yang <cafferychen7850@gmail.com>
2024-11-05 14:51:20 -0600

    feat: Add simulation function skeleton

---
+++ R/simulate_data.R
@@ -0,0 +1,18 @@
+#' Simulate Microbiome Data
+#' 
+#' @param n_samples Number of samples to simulate
+#' @param n_taxa Number of taxa to simulate
+#' @param dispersion Dispersion parameter for negative binomial distribution
+#' @param effect_size Effect size for differential abundance
+#'
+#' @return A simulated microbiome dataset
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' sim_data <- simulate_data(n_samples = 100, n_taxa = 50)
+#' }
+simulate_data <- function(n_samples, n_taxa, dispersion = 0.3, effect_size = 1) {
+    # TODO: Implement simulation logic
+    stop("Function not yet implemented")
+} 
