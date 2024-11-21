2d66473ae60f3b24f6958632027b893eec2258f6
Chen Yang <cafferychen7850@gmail.com>
2024-11-21 16:55:58 -0600

    fix: update package dependencies and global imports

--- R/globals.R
+++ R/globals.R
@@ -1,3 +1,10 @@
+# Import required functions from base packages
+#' @importFrom stats binom.test cor rbinom rnbinom rnorm sd setNames t.test
+#' @importFrom utils read.csv packageVersion
+#' @importFrom tidyselect all_of
+#' @importFrom plotly ggplotly layout
+#' @importFrom scales percent_format
+NULL
 # Define global variables used in package
 utils::globalVariables(c(
     "method",
@@ -8,5 +15,18 @@ utils::globalVariables(c(
     "metric",
     "value",
     "physeq",
-    "p_adjust_method"
+    "p_adjust_method",
+    "run_daa_method",
+    "cor",
+    "sd",
+    "binom.test",
+    "read.csv",
+    "all_of",
+    "rnorm",
+    "t.test",
+    "setNames",
+    "rnbinom",
+    "rbinom"
 )) 
+
+
